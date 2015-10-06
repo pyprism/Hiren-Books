0251fbd6a73951b71e14a59f7f8b2fc123480874
Ashutosh Das <git.pyprism@gmail.com>
2015-10-06 20:55:34 +0600

    add new field

--- book/models.py
+++ book/models.py
@@ -8,9 +8,10 @@ class Book(models.Model):
     name = models.CharField(max_length=400, blank=False, null=False)
     slug = models.CharField(max_length=150, unique=True)
     note = models.TextField()
+    finished = models.BooleanField(default=False)
     page_no = models.IntegerField(default=0)
-    pdf = models.FileField(upload_to='%d/%m/%Y')
-    date = models.DateField(auto_now_add=True, auto_now=True)
+    pdf = models.FileField(upload_to='hiren/%Y/%m/%d')
+    date = models.DateField(auto_now_add=True)
 
     def save(self):
         super().save()
