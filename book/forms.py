e32c13aad851d93629b0fffefc8da5be9ef0430f
Ashutosh Das <git.pyprism@gmail.com>
2015-10-06 20:54:28 +0600

    add new form

--- book/forms.py
+++ book/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from book.models import Book
+from django.forms import ModelForm
 
 
 class BookForms(forms.Form):
     class Meta:
         model = Book
-        fields = ['name', 'note', 'pdf']
+        fields = ['name', 'note']
+
+
+class AddForms(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'pdf']
