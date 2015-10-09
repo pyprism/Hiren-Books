ed0c7046e66df974e2bcd4ecb6751cceb697f755
Ashutosh Das <git.pyprism@gmail.com>
2015-10-09 22:05:45 +0600

    Add file validation

--- book/forms.py
+++ book/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from book.models import Book
 from django.forms import ModelForm
+import magic
 
 
 class BookForms(forms.Form):
@@ -10,6 +11,16 @@ class BookForms(forms.Form):
 
 
 class AddForms(ModelForm):
+
+    def clean_file(self):
+        pdf = self.cleaned_data['pdf']
+        mime = magic.from_buffer(pdf.read(), mime=True)
+        print(mime)
+        if not mime == 'application/pdf':
+            raise forms.ValidationError('File must be a PDF document')
+        else:
+            return pdf
+
     class Meta:
         model = Book
         fields = ['name', 'pdf']
