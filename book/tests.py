58eec815827019b30be468d5b009d17ab9dff016
Ashutosh Das <git.pyprism@gmail.com>
2015-10-12 19:04:04 +0600

    Add basic test

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,15 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test import LiveServerTestCase
+from django.contrib.auth.models import User
+from book.views import *
+from book.models import Book
+import os
 
-# Create your tests here.
+
+class IndexPageTest(TestCase):
+
+    def test_root_url_resolves_to_index_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
