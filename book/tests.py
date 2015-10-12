037ed9599849f2e6a9b5f74fea68180bd4ffa4d3
Ashutosh Das <git.pyprism@gmail.com>
2015-10-12 19:19:12 +0600

    add another test

--- book/tests.py
+++ book/tests.py
@@ -13,3 +13,7 @@ class IndexPageTest(TestCase):
     def test_root_url_resolves_to_index_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_uses_login_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'login.html')
