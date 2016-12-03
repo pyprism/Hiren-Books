89c8997f66a2b4971603d3062c25b9063d2dd74f
Ashutosh Das <git.pyprism@gmail.com>
2016-12-03 17:12:09 +0600

    removed unused lines

--- book/forms.py
+++ book/forms.py
@@ -1,19 +1,8 @@
-from django import forms
 from book.models import Book
 from django.forms import ModelForm
-import magic
 
 
 class AddForms(ModelForm):
-
-    def clean_file(self):
-        pdf = self.cleaned_data['pdf']
-        mime = magic.from_buffer(pdf.read(), mime=True)
-        print(mime)
-        if not mime == 'application/pdf':
-            raise forms.ValidationError('File must be a PDF document')
-        else:
-            return pdf
 
     class Meta:
         model = Book
