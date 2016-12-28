b3043ad63fc63cfe739a357b9be82128f5090226
Ashutosh Das <git.pyprism@gmail.com>
2016-12-28 21:46:06 +0600

    added new field

--- book/forms.py
+++ book/forms.py
@@ -6,4 +6,4 @@ class AddForms(ModelForm):
 
     class Meta:
         model = Book
-        fields = ['name', 'pdf', 'url', 'current_url']
+        fields = ['name', 'pdf', 'url', 'current_url', 'type', 'folder']
