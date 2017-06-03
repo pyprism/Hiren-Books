2163456f01ec7d0f6bc4e224e59cc5549ac1ecd8
Ashutosh Das <git.pyprism@gmail.com>
2017-06-03 14:56:18 +0600

    wip

--- book/forms.py
+++ book/forms.py
@@ -6,4 +6,5 @@ class AddForms(ModelForm):
 
     class Meta:
         model = Book
-        fields = ['name', 'pdf', 'url', 'current_url', 'type', 'folder']
+        # fields = ['name', 'pdf', 'url', 'current_url', 'type', 'folder']
+        exclude = ('slug', )
