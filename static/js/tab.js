870e5df2b012f08aaf3559e26dc29929afb475fe
Ashutosh Das <git.pyprism@gmail.com>
2017-06-06 08:50:33 +0600

    added new static js

---
+++ static/js/tab.js
@@ -0,0 +1,13 @@
+/**
+ * Created by prism on 10/10/15.
+ */
+/**
+ * function for jquery tab initialization
+ */
+$(function() {
+    $( "#tabs" ).tabs({
+        active: 2
+    });
+
+    $('#summernote').summernote({height: 200});
+});
